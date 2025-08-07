6c962b4daac64db7d03826c1f1a427fd8f038524
rohi5431 <rohitk60316@gmail.com>
2025-08-07 16:43:58 +0530

    server error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+require('dotenv').config();
 const methodOverride = require('method-override');
 
 const app = express();
