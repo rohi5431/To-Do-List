342225cd081926acc370a890144462d627705f5c
rohi5431 <187667367+rohi5431@users.noreply.github.com>
2025-09-21 23:45:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,23 +21,36 @@ mongoose.connect(process.env.MONGODB_URI,{
 
 const taskSchema = new mongoose.Schema({
   title: String,
-  priority: { type: String, enum: ["high", "medium", "low"], default: "low" }
+  priority: {
+    type: String,
+    enum: ["high", "medium", "low"],
+    default: "low" }
 });
 
 const Task = mongoose.model("Task", taskSchema);
 app.get("/", async (req, res) => {
   const tasks = await Task.find({});
-  res.render("index", { tasks, error: "", message: "" });
+  res.render("index", {
+    tasks,
+    error: "",
+    message: ""
+  });
 });
 
 app.post("/tasks", async (req, res) => {
   const { title, priority } = req.body;
   try{
-    await Task.create({ title, priority });
+    await Task.create({
+      title,
+      priority
+    });
     res.redirect("/");
   } 
   catch(err){
-    res.render("index", { tasks: await Task.find({}), error: "Error adding task", message: "" });
+    res.render("index", {
+      tasks: await Task.find({}),
+      error: "Error adding task", message: ""
+    });
   }
 });
 
@@ -45,11 +58,18 @@ app.post("/tasks/edit/:id", async (req, res) => {
   const { id } = req.params;
   const { title, priority } = req.body;
   try{
-    await Task.findByIdAndUpdate(id, { title, priority });
+      await Task.findByIdAndUpdate(id, {
+      title,
+      priority
+    });
     res.redirect("/");
   } 
   catch(err){
-    res.render("index", { tasks: await Task.find({}), error: "Error updating task", message: "" });
+    res.render("index", {
+      tasks: await Task.find({}),
+      error: "Error updating task",
+      message: ""
+    });
   }
 });
 
@@ -60,9 +80,16 @@ app.post("/tasks/delete/:id", async (req, res) => {
     res.redirect("/");
   } 
   catch(err){
-    res.render("index", { tasks: await Task.find({}), error: "Error deleting task", message: "" });
+    res.render("index", { 
+      tasks: await Task.find({}),
+      error: "Error deleting task",
+      message: ""
+    });
   }
 });
 
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+app.listen(3000, function(){
+    console.log("Server running on http://localhost:3000");
+});
+
