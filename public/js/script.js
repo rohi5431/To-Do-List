80eb5d027168fb8b8e1915b5cb7c3df6dd5d9f6b
rohi5431 <187667367+rohi5431@users.noreply.github.com>
2025-09-22 19:10:42 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
-const taskList = document.querySelector(".task-list");
+const task = document.querySelector(".task-list");
 
-taskList.addEventListener("click", function(e){
+task.addEventListener("click", function(e){
   if(!e.target.classList.contains("edit-btn")){
      return;
   }
@@ -19,15 +19,15 @@ taskList.addEventListener("click", function(e){
     form.className = "edit-form";
     form.method = "POST";
     form.action = "/tasks/edit/" + id
-    var highSelected = priority === "high" ? "selected" : "";
-    var mediumSelected = priority === "medium" ? "selected" : "";
-    var lowSelected = priority === "low" ? "selected" : ""
+    var high = priority === "high" ? "selected" : "";
+    var medium = priority === "medium" ? "selected" : "";
+    var low = priority === "low" ? "selected" : ""
     form.innerHTML = `
       <input type="text" name="title" value="${title}" required>
       <select name="priority" required>
-         <option value="high" ${highSelected}>High</option>
-         <option value="medium" ${mediumSelected}>Medium</option>
-         <option value="low" ${lowSelected}>Low</option>
+         <option value="high" ${high}>High</option>
+         <option value="medium" ${medium}>Medium</option>
+         <option value="low" ${low}>Low</option>
       </select>
       <button type="submit">Update</button>
       <button type="button" class="cancel-edit">Cancel</button>
@@ -35,8 +35,8 @@ taskList.addEventListener("click", function(e){
 
   li.insertAdjacentElement("afterend", form);
 
-  var cancelBtn = form.querySelector(".cancel-edit");
-    cancelBtn.addEventListener("click", function(){
+  var cancel = form.querySelector(".cancel-edit");
+    cancel.addEventListener("click", function(){
     form.remove();
   });
   });
